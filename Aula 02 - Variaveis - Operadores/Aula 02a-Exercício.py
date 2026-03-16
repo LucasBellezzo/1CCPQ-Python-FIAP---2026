49ebfe15d56b58bd4eefd8b980d16dd165493c54
LucasBellezzo <bellezzolucas@gmail.com>
2026-03-16 20:45:48 -0300

    Aula 2 - Variaveis

---
+++ Aula 02 - Variaveis - Operadores/Aula 02a-Exercício.py
@@ -0,0 +1,22 @@
+
+# DESAFIO 1
+
+nome = input('Digite seu nome ')
+print(f"Seja bem vindo, {nome}! ")
+
+
+# DESAFIO 2
+
+mes = input('Em que mês você nasceu? ')
+ano = input('Em qual ano você nasceu? ')
+
+print(f'Você nasceu em {mes}, no ano de {ano}! ')
+
+
+# DESAFIO 3
+
+n1 = int(input('Digite um número '))
+n2 = int(input('Digite outro Número '))
+
+print(n1+n2)
+
---
+++ Aula 02 - Variaveis - Operadores/Aula 02a-Variaveis.py
@@ -0,0 +1,31 @@
+print("Hello, World!")
+
+print(7+4)
+
+print("7"+"4") # Concatenando Strings
+
+
+# Isso é um comentário de 1 linha
+
+'''
+Isso é 
+um comentário de
+multiplas 
+linhas
+'''
+
+# Variáveis
+nome = "Lucas"
+idade = "23"
+peso = "70"
+
+print(f'Olá, {nome}!')
+
+# INPUTS --- SIMULAÇÃO DE UM FORMS NO CMD
+
+nome = input('Qual é o seu nome? ')
+idade = input('Qual é sua idade? ')
+peso = input('Qual é seu peso? ')
+
+print(f'Seu nome é {nome}, você tem {idade} anos e pesa {peso} KG')
+
