a9502906b198afe44202e3817d0af6663ff05143
LucasBellezzo <bellezzolucas@gmail.com>
2026-04-27 21:24:04 -0300

    27/4/26

---
+++ Aula04_Estruturas_de_repetição/Repetição_For(Para cada...).py
@@ -0,0 +1,16 @@
+for count_music in range(3):
+    print(f"Música {count_music}")
+
+# ------------------------------------------------------------------------------------ #
+
+# de 1 até 11, pulando de 2 em 2
+
+for i in range(1,12, 2):
+    print(i)
+
+# ------------------------------------------------------------------------------------ #
+
+playlist = int(input("Digite a quantidade de músicas que você tem na playlist (DB): "))
+
+for i in range(playlist):
+    print(f"Música {i}")
---
+++ Aula04_Estruturas_de_repetição/Repetição_encadeada.py
@@ -0,0 +1,10 @@
+# PS = Caso queiramos fazer uma tabela com PNG, Title, Artist e nas Colunas o nome das músicas: usamos i para linhas e j para colunas
+
+
+# LAÇOS ALINHADOS
+# REPETIÇÕES ENCADEADAS
+
+for i in range(0,3):
+    for j in range(0, 3, 2):
+        print(f"i:{i}, j:{j}")
+
